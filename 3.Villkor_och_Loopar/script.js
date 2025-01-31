97c0ade6ad7dadfc1eaf59495565f1d32b15a8d1
Hugo <hugo.benjaminsson1@gmail.com>
2025-01-31 11:15:42 +0100

    display updates

--- 3.Villkor_och_Loopar/script.js
+++ 3.Villkor_och_Loopar/script.js
@@ -1,10 +1,10 @@
-alert("check the console for awesome features");
+alert("Check the console for awesome features");
 
 const rndValue = Math.floor(Math.random() * 10);
 
-console.log("This is a random value" + rndValue);
+console.log("This is a random value: " + rndValue);
 
-console.log("Now i will count from 1-10")
-for (let i = 0; i < 10; i++) {
+console.log("\nNow i will count from 1-10")
+for (let i = 1; i <= 10; i++) {
     console.log(i);
 }
