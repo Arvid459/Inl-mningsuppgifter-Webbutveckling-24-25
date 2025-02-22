eddbc51f86a21a1aca19cd52778d244e6426a80a
Hugo <hugo.benjaminsson1@gmail.com>
2025-02-22 09:28:08 +0100

    uppgift 6 continue

--- 6.Arrays_och_Objekt/script.js
+++ 6.Arrays_och_Objekt/script.js
@@ -1,6 +1,27 @@
 
 const names = ["Ville", "Alla"];
+const toDo = [""];
 
-names.forEach((name, index) =>{
-    console.log(`${index + 1}: ${name}`);
-});
+    array.forEach((name, index) =>{
+        console.log(`${index + 1}: ${name}`);
+    }); 
+
+    function print(input){
+        const newDiv = document.createElement("div");
+        newDiv;
+        newDiv.appendChild(document.createTextNode(`${input}`));
+        
+        const button = document.createElement("button");
+        newDiv.appendChild(button);
+        button.textContent = "Remove";
+        document.body.appendChild(newDiv);
+
+    }
+
+
+function add(){
+    let input = document.querySelector('input').value;
+    toDo.push(input);
+    print(input);
+}
+
