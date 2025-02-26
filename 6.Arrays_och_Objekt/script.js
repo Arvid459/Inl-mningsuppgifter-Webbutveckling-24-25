bd1c1e5557c09c7dc84ea9e3e68def14e620eb9d
Hugo <hugo.benjaminsson1@gmail.com>
2025-02-26 14:41:12 +0100

    up 6 is ready

--- 6.Arrays_och_Objekt/script.js
+++ 6.Arrays_och_Objekt/script.js
@@ -1,27 +1,41 @@
 
 const names = ["Ville", "Alla"];
-const toDo = [""];
+const toDo = [];
 
-    array.forEach((name, index) =>{
+    names.forEach((name, index) =>{
         console.log(`${index + 1}: ${name}`);
     }); 
 
     function print(input){
+        if (input != "" && !toDo.includes(input)){
         const newDiv = document.createElement("div");
         newDiv;
         newDiv.appendChild(document.createTextNode(`${input}`));
         
         const button = document.createElement("button");
-        newDiv.appendChild(button);
         button.textContent = "Remove";
+        button.onclick = function(){
+            remove(newDiv);
+        };
+
+        newDiv.appendChild(button);
         document.body.appendChild(newDiv);
-
+        toDo.push(input);
     }
-
+    else if (toDo.includes(input)){
+        alert("This chore is alredy on your list");
+    }
+    else{
+        alert("You need to enter a chore");
+    }
+    }
+function remove(element){
+    element.remove();
+    toDo.pop(element);
+}
 
 function add(){
     let input = document.querySelector('input').value;
-    toDo.push(input);
     print(input);
 }
 
