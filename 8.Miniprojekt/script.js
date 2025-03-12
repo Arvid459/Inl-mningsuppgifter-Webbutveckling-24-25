c7859b96fe52c0e1594fb874ed1116554a137a72
Hugo <hugo.benjaminsson1@gmail.com>
2025-03-12 15:02:28 +0100

    up 8 update

--- 8.Miniprojekt/script.js
+++ 8.Miniprojekt/script.js
@@ -1,10 +1,40 @@
-function cookie(){
-    localStorage = document.querySelector('input').value;
+const radio = document.getElementById("myRadio");
+const username = document.getElementById("username");
+const password = document.getElementById("password");
+const pressedKeys = document.getElementById("pressedKeys");
+
+
+function saveInformation(){
+    if (username.value.trim() === "" || password.value.trim() === ""){
+        alert("You need to enter both username and password");
+    }
+    else{
+        localStorage.setItem("username", username.value);
+        localStorage.setItem("password", password.value);
+    }
 }
 
-if (localStorage == 'true'){
-    document.querySelector('input').value = 'true';
-}
+document.addEventListener("DOMContentLoaded", function () {
 
-//localStorage
-//sessionStorage 
+    console.log(sessionStorage.getItem("radioChecked"));
+
+    if (sessionStorage.getItem("radioChecked") === "true") {
+        radio.checked = sessionStorage.getItem("radioChecked");
+    }
+
+    if (localStorage.getItem("username") && localStorage.getItem("password")){
+        username.value = localStorage.getItem("username");
+        password.value = localStorage.getItem("password");
+    }
+
+    // Store the checked state when clicked
+    radio.addEventListener("click", function () {
+        sessionStorage.setItem("radioChecked", radio.checked);
+    });
+
+});
+
+
+
+
+//session
